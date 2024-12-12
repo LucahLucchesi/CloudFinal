d0cdc480405b1a25536b5d5c6c000911b1a554b0
Lucchesi <lucchesil1011@my.uwstout.edu>
2024-12-11 20:48:47 -0600

    test commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('CNIT 371 Final!'));
+app.get('/', (req, res) => res.send('Test!'));
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
